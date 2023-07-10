e3b8e5bce0bb837429d176dc208c33c786544397
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-07-10 15:44:31 +0530

    done with backend server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const morgan = require('morgan')
+require('colores')
+
+const app = express()
+
+//middlewares
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello from node server</h1>")
+})
+
+app.listen(8080, () => {
+    console.log("Serever are running")
+})
