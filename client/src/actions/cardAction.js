f7f51fd4f2760d229f0ec943f08e33864bbc5ac5
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-07-11 12:02:34 +0530

    addint oin data localStore

--- client/src/actions/cardAction.js
+++ client/src/actions/cardAction.js
@@ -1,4 +1,4 @@
-export const addToCard = (pizza, quantity, varinet) => (dispatch) => {
+export const addToCard = (pizza, quantity, varinet) => (dispatch, getState) => {
     var carItem = {
         name: pizza.name,
         _id: pizza._id,
@@ -8,5 +8,5 @@ export const addToCard = (pizza, quantity, varinet) => (dispatch) => {
         price: pizza.prices[0][varinet] * quantity,
     };
     dispatch({ type: "ADD_TO_CART", payload: carItem });
-
+    localStorage.setItem('cartItems', JSON.stringify(getState().cartReducer.carItems));
 };
--- client/src/store.js
+++ client/src/store.js
@@ -4,12 +4,21 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { getAllPizzaReducer } from './reducers/pizzaReducer'
 import { cartReducer } from './reducers/cartReducer'
 
+
+const cartItems = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem("cartItems")) : [];
+
 const rootReducer = combineReducers({
     getAllPizzaReducer: getAllPizzaReducer,
     cartReducer: cartReducer,
 })
 
-const intialState = {}
+
+const intialState = {
+    cartReducer: {
+        cartItems: cartItems,
+    }
+}
 const middleware = [thunk]
 
 const store = createStore(rootReducer, intialState, composeWithDevTools(applyMiddleware(...middleware)))
