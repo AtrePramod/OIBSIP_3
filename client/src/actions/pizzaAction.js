1833ea26e15666802fc8f968b890cc01f060b729
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-07-13 13:04:28 +0530

    clear console

--- client/src/actions/pizzaAction.js
+++ client/src/actions/pizzaAction.js
@@ -3,7 +3,7 @@ export const getAllPizzas = () => async dispatch => {
     dispatch({ type: 'GET_PIZZAS_REQUEST' })
     try {
         const res = await axios.get('/api/pizzas/getAllPizzas')
-        console.log(res);
+
         dispatch({ type: 'GET_PIZZAS_SUCCESS', payload: res.data })
 
     } catch (error) {
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -10,7 +10,7 @@ router.post('/register', (req, res) => {
         newUser.save()
         res.status(200).json({ success: true, message: 'Register success' })
     } catch (error) {
-        res.status(400).json({ message: `${error}." pramodd"`, })
+        res.status(400).json({ message: `${error} `, })
 
     }
 });
