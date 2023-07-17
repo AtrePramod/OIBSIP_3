24172525bedb8be4a153f4f1485c33b4d037b788
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-07-17 19:55:49 +0530

    adding payment in database

---
+++ server/models/orderModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "order name required"]
+
+    },
+    email: {
+        type: String,
+        required: [true, "order email required"]
+
+    },
+    userid: {
+        type: String,
+        //  required: true
+
+    },
+
+    orderitems: [],
+    shippingAddress: {
+        type: String,
+        required: true
+    },
+    orderAmount: {
+        type: String,
+        // required: true
+    },
+    isDelivered: {
+        type: String,
+        //  required: true
+    },
+    transectionId: {
+        type: String,
+        //  required: true
+    },
+
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('Order', orderSchema)
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { v4: uuidv4 } = require('uuid')
 const stripe = require('stripe')('sk_test_51NTja5SCMaxcwsjuhj0q0J55QirscfXaetfNNTc8d1Uas7p2frBxV66QAVYl0pShGD9jg3sRySuGPx37rzToO8sd002iDTcLKD')
-
+const Order = require("../models/orderModel")
 
 router.post('/placeorder', async (req, res) => {
     const { token, subTotal, currentUser, cartItems } = req.body;
@@ -21,6 +21,23 @@ router.post('/placeorder', async (req, res) => {
             idempotencyKey: uuidv4(),
         });
         if (payment) {
+
+            const newOrder = new Order({
+                name: currentUser.name,
+                email: currentUser.email,
+                userid: currentUser._id,
+                orderItems: cartItems,
+                orderAmount: subTotal,
+                shippingAddress: {
+                    street: token.card.address_line1,
+                    city: token.card.address_city,
+                    country: token.card.address_country,
+                    picode: token.card.address_zip
+                },
+                transectionId: payment.source.id,
+
+            })
+            newOrder.save();
             res.send('Payment Success')
         }
         else {
